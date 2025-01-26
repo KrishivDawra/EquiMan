2bc722c15fddfe1ccfea53e6d970f96b21daf906
KrishivDawra <krishivdawra@gmail.com>
2025-01-26 17:18:51 +0530

    first commit

---
+++ src/components/Body.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const Body = () => {
+  return (
+    <div>
+      <div className='flex py-14  '>
+        <div className=' space-y-4 pt-[80px] pr-[50px]'>
+          <h1 className='text-5xl font-sans text-green-600 font-semibold'>Farm-Fresh Organic Eggs, Straight to Your Door!</h1>
+          <p className='font-sans '>Get farm-fresh eggs delivered to your doorstep at unbeatable wholesale prices – Quality and convenience, straight from the farm!</p>
+          <button className='bg-green-400 hover:bg-green-600 px-8 py-2 rounded-full font-semibold'>Join Us & Place Your ORDER NOW!</button>
+        </div>
+
+        <img src="./src/assets/eggnest.jpg" className='h-[450px]' alt="" />
+      </div>
+
+      <div>
+        <div>
+
+        </div>
+        
+        <div>
+
+        </div>
+
+        <div>
+          
+        </div>
+      </div>
+    </div>
+    
+    
+
+
+    
+  )
+}
+
+export default Body
