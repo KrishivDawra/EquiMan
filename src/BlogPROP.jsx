c775f69509e9af7887d395140bec47ee4355480b
KrishivDawra <krishivdawra@gmail.com>
2025-01-28 08:52:52 +0530

    shadow in blog

--- src/BlogPROP.jsx
+++ src/BlogPROP.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const BlogsPROP = (props) => {
   return (
-    <div className='flex flex-col h-68 max-w-3xl border-4 border-black p-2'>
+    <div className='flex flex-col h-68 max-w-3xl border-4 shadow-xl border-black p-2'>
         <h1 className='text-lg font-bold font-serif text-blue-700'>Author: {props.author}</h1>
         <h1 className='text-md font-serif'>Title: {props.title}</h1>
         <h1 className='text-blue-700 text-lg font-bold'>Description:</h1>
